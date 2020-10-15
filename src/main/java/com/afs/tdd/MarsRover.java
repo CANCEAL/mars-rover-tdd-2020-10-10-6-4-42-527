9919b185fdd1a232e8b254d46950f6bcbf570ee7
CANCEAL <alfred.cancejo@oocl.com>
2020-10-15 17:20:16 +0800

    [Mars Rover] [Alfred] Feature: code to return new position when given is x 0, y 0, head N, and command R.

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -18,6 +18,15 @@ public class MarsRover {
         if (command.equals("L")) {
             turnLeft();
         }
+        if (command.equals("R")) {
+            turnRight();
+        }
+    }
+
+    private void turnRight() {
+        if (heading.equals("N")) {
+            heading = "E";
+        }
     }
 
     private void turnLeft() {
