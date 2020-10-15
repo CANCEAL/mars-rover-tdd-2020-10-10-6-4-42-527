89ad94a4cbb176f6d7030f243452d3ad0457b5f5
CANCEAL <alfred.cancejo@oocl.com>
2020-10-15 18:49:27 +0800

    [Mars Rover] [Alfred] Test: case return new position when given is x 0, y 0, head S, and command M.

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -5,18 +5,12 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MarsRoverTest {
-    public void newPosition(int x, int y, String heading) {
-        
-    }
-
     @Test
     void should_return_x_0_y_1_heading_N_when_execute_command_given_x_0_y_0_heading_n_and_command_M() {
         //given
         MarsRover marsRover = new MarsRover(0, 0, "N");
-
         //when
-        marsRover.executeCommands("M");
-
+        marsRover.executeCommand("M");
         //then
         assertEquals(0, marsRover.getLocationX());
         assertEquals(1, marsRover.getLocationY());
@@ -27,10 +21,8 @@ class MarsRoverTest {
     void should_return_x_0_y_0_heading_W_when_execute_command_given_x_0_y_0_heading_n_and_command_L() {
         //given
         MarsRover marsRover = new MarsRover(0, 0, "N");
-
         //when
-        marsRover.executeCommands("L");
-
+        marsRover.executeCommand("L");
         //then
         assertEquals(0, marsRover.getLocationX());
         assertEquals(0, marsRover.getLocationY());
@@ -41,13 +33,25 @@ class MarsRoverTest {
     void should_return_x_0_y_0_heading_W_when_execute_command_given_x_0_y_0_heading_n_and_command_R() {
         //given
         MarsRover marsRover = new MarsRover(0, 0, "N");
-
         //when
-        marsRover.executeCommands("R");
-
+        marsRover.executeCommand("R");
         //then
         assertEquals(0, marsRover.getLocationX());
         assertEquals(0, marsRover.getLocationY());
         assertEquals("E", marsRover.getHeading());
     }
+
+    @Test
+    void should_return_x_0_y_negative_1_heading_S_when_execute_command_given_x_0_y_0_heading_S_and_command_M() {
+        //given
+        MarsRover marsRover = new MarsRover(0, 0, "S");
+        //when
+        marsRover.executeCommand("M");
+        //then
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(-1, marsRover.getLocationY());
+        assertEquals("S", marsRover.getHeading());
+    }
+
+
 }
