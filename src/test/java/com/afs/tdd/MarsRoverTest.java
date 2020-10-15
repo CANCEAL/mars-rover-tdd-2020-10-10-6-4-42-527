adc78f49fa1ee5fc62f8ed528afbce4863b3a63d
CANCEAL <alfred.cancejo@oocl.com>
2020-10-15 17:11:54 +0800

    [Mars Rover] [Alfred] Test: case return new position when given is x 0, y 0, head N, and command L.

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -4,7 +4,11 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class DemoTest {
+class MarsRoverTest {
+    public void newPosition(int x, int y, String heading) {
+
+    }
+
     @Test
     void should_return_x_0_y_1_heading_N_when_execute_command_given_x_0_y_0_heading_n_and_command_M() {
         //given
@@ -17,5 +21,19 @@ class DemoTest {
         assertEquals(0, marsRover.getLocationX());
         assertEquals(1, marsRover.getLocationY());
         assertEquals("N", marsRover.getHeading());
+    }
+
+    @Test
+    void should_return_x_0_y_0_heading_W_when_execute_command_given_x_0_y_0_heading_n_and_command_L() {
+        //given
+        MarsRover marsRover = new MarsRover(0, 0, "N");
+
+        //when
+        marsRover.executeCommands("L");
+
+        //then
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(0, marsRover.getLocationY());
+        assertEquals("W", marsRover.getHeading());
     }
 }
