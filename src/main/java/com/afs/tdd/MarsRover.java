1199d9beaa8a099cf35c14837a9119989429f202
CANCEAL <alfred.cancejo@oocl.com>
2020-10-15 17:07:53 +0800

    [Mars Rover] [Alfred] Feature: code to return new position when given is x 0, y 0, head N, and command M.

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -1,21 +1,37 @@
 package com.afs.tdd;
 
 public class MarsRover {
-    public MarsRover(int i, int i1, String n) {
+    private final int locationX;
+    private int locationY;
+    private final String heading;
+
+    public MarsRover(int locationX, int locationY, String heading) {
+        this.locationX = locationX;
+        this.locationY = locationY;
+        this.heading = heading;
     }
 
-    public void executeCommands(String m) {
+    public void executeCommands(String command) {
+        if (command.equals("M")) {
+            move();
+        }
+    }
+
+    private void move() {
+        if (heading.equals("N")) {
+            locationY += 1;
+        }
     }
 
     public int getLocationX() {
-        return 0;
+        return locationX;
     }
 
     public int getLocationY() {
-        return 0;
+        return locationY;
     }
 
     public String getHeading() {
-        return null;
+        return heading;
     }
 }
