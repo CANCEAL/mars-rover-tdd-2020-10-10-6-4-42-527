6b94148830a15fe4be10b26a0fddf89ccf77b1e5
CANCEAL <alfred.cancejo@oocl.com>
2020-10-15 18:58:11 +0800

    [Mars Rover] [Alfred] Test: case to return new position when given is x 0, y 0, head S, and command R.

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -65,5 +65,15 @@ class MarsRoverTest {
         assertEquals("E", marsRover.getHeading());
     }
 
-
+    @Test
+    void should_return_x_0_y_0_heading_W_when_execute_command_given_x_0_y_0_heading_S_and_command_R() {
+        //given
+        MarsRover marsRover = new MarsRover(0, 0, "S");
+        //when
+        marsRover.executeCommand("R");
+        //then
+        assertEquals(0, marsRover.getLocationX());
+        assertEquals(0, marsRover.getLocationY());
+        assertEquals("W", marsRover.getHeading());
+    }
 }
