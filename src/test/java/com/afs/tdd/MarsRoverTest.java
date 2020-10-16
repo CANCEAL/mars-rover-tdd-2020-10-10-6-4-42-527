24baa2f45c13153a0cc7225d883dd98e6c376329
CANCEAL <alfred.cancejo@oocl.com>
2020-10-16 11:15:53 +0800

    [Mars Rover] [Alfred] Refactor: update function name to getNewPosition

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class MarsRoverTest {
-    public void newPosition(int locationX, int locationY, String heading, String command, int expectedLocationX, int expectedLocationY, String expectedHeading) throws CommandNotDefinedException {
+    private void getNewPosition(int locationX, int locationY, String heading, String command, int expectedLocationX, int expectedLocationY, String expectedHeading) throws CommandNotDefinedException {
         //given
         MarsRover marsRover = new MarsRover(locationX, locationY, heading);
         //when
@@ -19,73 +19,76 @@ class MarsRoverTest {
 
     @Test
     void should_return_x_0_y_1_heading_N_when_execute_command_given_x_0_y_0_heading_n_and_command_M() throws CommandNotDefinedException {
-        newPosition(0, 0, "N", "M", 0, 1, "N");
+        getNewPosition(0, 0, "N", "M", 0, 1, "N");
     }
 
     @Test
     void should_return_x_0_y_0_heading_W_when_execute_command_given_x_0_y_0_heading_n_and_command_L() throws CommandNotDefinedException {
-        newPosition(0, 0, "N", "L", 0, 0, "W");
+        getNewPosition(0, 0, "N", "L", 0, 0, "W");
     }
 
     @Test
     void should_return_x_0_y_0_heading_W_when_execute_command_given_x_0_y_0_heading_n_and_command_R() throws CommandNotDefinedException {
-        newPosition(0, 0, "N", "R", 0, 0, "E");
+        getNewPosition(0, 0, "N", "R", 0, 0, "E");
     }
 
     @Test
     void should_return_x_0_y_negative_1_heading_S_when_execute_command_given_x_0_y_0_heading_S_and_command_M() throws CommandNotDefinedException {
-        newPosition(0, 0, "S", "M", 0, -1, "S");
+        getNewPosition(0, 0, "S", "M", 0, -1, "S");
     }
 
     @Test
     void should_return_x_0_y_0_heading_E_when_execute_command_given_x_0_y_0_heading_S_and_command_L() throws CommandNotDefinedException {
-        newPosition(0, 0, "S", "L", 0, 0, "E");
+        getNewPosition(0, 0, "S", "L", 0, 0, "E");
     }
 
     @Test
     void should_return_x_0_y_0_heading_W_when_execute_command_given_x_0_y_0_heading_S_and_command_R() throws CommandNotDefinedException {
-        newPosition(0, 0, "S", "R", 0, 0, "W");
+        getNewPosition(0, 0, "S", "R", 0, 0, "W");
     }
 
     @Test
     void should_return_x_1_y_0_heading_E_when_execute_command_given_x_0_y_0_heading_E_and_command_M() throws CommandNotDefinedException {
-        newPosition(0, 0, "E", "M", 1, 0, "E");
+        getNewPosition(0, 0, "E", "M", 1, 0, "E");
     }
 
     @Test
     void should_return_x_0_y_0_heading_N_when_execute_command_given_x_0_y_0_heading_E_and_command_L() throws CommandNotDefinedException {
-        newPosition(0, 0, "E", "L", 0, 0, "N");
+        getNewPosition(0, 0, "E", "L", 0, 0, "N");
     }
 
     @Test
     void should_return_x_0_y_0_heading_S_when_execute_command_given_x_0_y_0_heading_E_and_command_R() throws CommandNotDefinedException {
-        newPosition(0, 0, "E", "R", 0, 0, "S");
+        getNewPosition(0, 0, "E", "R", 0, 0, "S");
     }
 
     @Test
     void should_return_x_negative_1_y_0_heading_W_when_execute_command_given_x_0_y_0_heading_W_and_command_M() throws CommandNotDefinedException {
-        newPosition(0, 0, "W", "M", -1, 0, "W");
+        getNewPosition(0, 0, "W", "M", -1, 0, "W");
     }
 
     @Test
     void should_return_x_0_y_0_heading_S_when_execute_command_given_x_0_y_0_heading_W_and_command_L() throws CommandNotDefinedException {
-        newPosition(0, 0, "W", "L", 0, 0, "S");
+        getNewPosition(0, 0, "W", "L", 0, 0, "S");
     }
 
     @Test
     void should_return_x_0_y_0_heading_N_when_execute_command_given_x_0_y_0_heading_W_and_command_R() throws CommandNotDefinedException {
-        newPosition(0, 0, "W", "R", 0, 0, "N");
+        getNewPosition(0, 0, "W", "R", 0, 0, "N");
     }
 
     @Test
     void should_return_x_negative_1_y_0_heading_N_when_execute_command_given_x_0_y_0_heading_n_and_command_MLMR() throws CommandNotDefinedException {
-        newPosition(0, 0, "N", "MLMR", -1, 1, "N");
+        getNewPosition(0, 0, "N", "MLMR", -1, 1, "N");
     }
 
     @Test
-    void should_return_CommandNotDefinedException_when_execute_command_given_x_0_y_0_heading_n_and_command_MLMRF() {
+    void should_return_CommandNotDefinedException_when_execute_command_given_x_0_y_0_heading_n_and_command_MLMRF() throws CommandNotDefinedException {
+        //given
         MarsRover marsRover = new MarsRover(0, 0, "N");
+        //then
         assertThrows(CommandNotDefinedException.class, () -> {
+            //when
             marsRover.isValidCommand("MLMRF");
         });
     }
