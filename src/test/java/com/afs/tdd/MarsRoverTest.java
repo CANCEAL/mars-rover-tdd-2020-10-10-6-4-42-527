e5908695a99c81493877400122f9ca59b0cb9557
CANCEAL <alfred.cancejo@oocl.com>
2020-10-18 00:03:09 +0800

    [Mars Rover] [Alfred] Refactor: update newPosition function name to getMarsRoverNewPosition

--- src/test/java/com/afs/tdd/MarsRoverTest.java
+++ src/test/java/com/afs/tdd/MarsRoverTest.java
@@ -16,7 +16,7 @@ class MarsRoverTest {
         locationY = 0;
     }
 
-    private void getNewPosition(String heading, String command, int expectedLocationX, int expectedLocationY, String expectedHeading) throws CommandNotDefinedException {
+    private void getMarsRoverNewPosition(String heading, String command, int expectedLocationX, int expectedLocationY, String expectedHeading) throws CommandNotDefinedException {
         //given
         MarsRover marsRover = new MarsRover(locationX, locationY, heading);
         //when
@@ -29,67 +29,67 @@ class MarsRoverTest {
 
     @Test
     void should_return_x_0_y_1_heading_N_when_execute_command_given_x_0_y_0_heading_n_and_command_M() throws CommandNotDefinedException {
-        getNewPosition("N", "M", 0, 1, "N");
+        getMarsRoverNewPosition("N", "M", 0, 1, "N");
     }
 
     @Test
     void should_return_x_0_y_0_heading_W_when_execute_command_given_x_0_y_0_heading_n_and_command_L() throws CommandNotDefinedException {
-        getNewPosition("N", "L", 0, 0, "W");
+        getMarsRoverNewPosition("N", "L", 0, 0, "W");
     }
 
     @Test
     void should_return_x_0_y_0_heading_W_when_execute_command_given_x_0_y_0_heading_n_and_command_R() throws CommandNotDefinedException {
-        getNewPosition("N", "R", 0, 0, "E");
+        getMarsRoverNewPosition("N", "R", 0, 0, "E");
     }
 
     @Test
     void should_return_x_0_y_negative_1_heading_S_when_execute_command_given_x_0_y_0_heading_S_and_command_M() throws CommandNotDefinedException {
-        getNewPosition( "S", "M", 0, -1, "S");
+        getMarsRoverNewPosition( "S", "M", 0, -1, "S");
     }
 
     @Test
     void should_return_x_0_y_0_heading_E_when_execute_command_given_x_0_y_0_heading_S_and_command_L() throws CommandNotDefinedException {
-        getNewPosition("S", "L", 0, 0, "E");
+        getMarsRoverNewPosition("S", "L", 0, 0, "E");
     }
 
     @Test
     void should_return_x_0_y_0_heading_W_when_execute_command_given_x_0_y_0_heading_S_and_command_R() throws CommandNotDefinedException {
-        getNewPosition( "S", "R", 0, 0, "W");
+        getMarsRoverNewPosition( "S", "R", 0, 0, "W");
     }
 
     @Test
     void should_return_x_1_y_0_heading_E_when_execute_command_given_x_0_y_0_heading_E_and_command_M() throws CommandNotDefinedException {
-        getNewPosition("E", "M", 1, 0, "E");
+        getMarsRoverNewPosition("E", "M", 1, 0, "E");
     }
 
     @Test
     void should_return_x_0_y_0_heading_N_when_execute_command_given_x_0_y_0_heading_E_and_command_L() throws CommandNotDefinedException {
-        getNewPosition( "E", "L", 0, 0, "N");
+        getMarsRoverNewPosition( "E", "L", 0, 0, "N");
     }
 
     @Test
     void should_return_x_0_y_0_heading_S_when_execute_command_given_x_0_y_0_heading_E_and_command_R() throws CommandNotDefinedException {
-        getNewPosition( "E", "R", 0, 0, "S");
+        getMarsRoverNewPosition( "E", "R", 0, 0, "S");
     }
 
     @Test
     void should_return_x_negative_1_y_0_heading_W_when_execute_command_given_x_0_y_0_heading_W_and_command_M() throws CommandNotDefinedException {
-        getNewPosition( "W", "M", -1, 0, "W");
+        getMarsRoverNewPosition( "W", "M", -1, 0, "W");
     }
 
     @Test
     void should_return_x_0_y_0_heading_S_when_execute_command_given_x_0_y_0_heading_W_and_command_L() throws CommandNotDefinedException {
-        getNewPosition( "W", "L", 0, 0, "S");
+        getMarsRoverNewPosition( "W", "L", 0, 0, "S");
     }
 
     @Test
     void should_return_x_0_y_0_heading_N_when_execute_command_given_x_0_y_0_heading_W_and_command_R() throws CommandNotDefinedException {
-        getNewPosition( "W", "R", 0, 0, "N");
+        getMarsRoverNewPosition( "W", "R", 0, 0, "N");
     }
 
     @Test
     void should_return_x_negative_1_y_0_heading_N_when_execute_command_given_x_0_y_0_heading_n_and_command_MLMR() throws CommandNotDefinedException {
-        getNewPosition( "N", "MLMR", -1, 1, "N");
+        getMarsRoverNewPosition( "N", "MLMR", -1, 1, "N");
     }
 
     @Test
